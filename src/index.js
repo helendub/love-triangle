29e9773771a7764d4fe0b7f2cbeaf1bceef3fa20
Alena Chyrskaya <helen.dubinskaya@gmail.com>
2019-02-27 18:13:06 +0100

    feat: new function for counting love triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  
+    let length = preferences.length; 
+    let count = 0;  
+      for (let i = 0; i<length; i++) { 
+        let n = i+1; 
+        let lover31 = preferences[i]; 
+        let lover12 = preferences[lover31-1]; 
+        let lover23 = preferences[lover12-1]; 
+
+        if (n==lover23)  {count++;}
+      }
+   return Math.floor(count/3);
 };
